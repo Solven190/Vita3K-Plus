a79fa2c28d391bd3df2f5563ef52a6a83ab346f1
nckstwrt <nick.g.stewart@gmail.com>
2026-07-02 11:19:41 +0100

    Cleaner Helldivers fix. Formatting

--- vita3k/renderer/src/vulkan/pipeline_cache.cpp
+++ vita3k/renderer/src/vulkan/pipeline_cache.cpp
@@ -599,8 +599,7 @@ vk::RenderPass PipelineCache::retrieve_render_pass(vk::Format format, bool force
         .srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eLateFragmentTests,
         .dstStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eEarlyFragmentTests,
         .srcAccessMask = vk::AccessFlagBits::eColorAttachmentWrite | vk::AccessFlagBits::eDepthStencilAttachmentWrite,
-        .dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite | 
-                         vk::AccessFlagBits::eDepthStencilAttachmentRead | vk::AccessFlagBits::eDepthStencilAttachmentWrite
+        .dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite | vk::AccessFlagBits::eDepthStencilAttachmentRead | vk::AccessFlagBits::eDepthStencilAttachmentWrite
     };
 
     // BUG: This is dead code (as immediately overwritten below)
