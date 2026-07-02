3776c598d46026216367036ee6146f3fd997c3dd
nckstwrt <nick.g.stewart@gmail.com>
2026-07-02 10:21:52 +0100

    Minor vulkan validation fix

--- vita3k/renderer/src/vulkan/pipeline_cache.cpp
+++ vita3k/renderer/src/vulkan/pipeline_cache.cpp
@@ -599,9 +599,11 @@ vk::RenderPass PipelineCache::retrieve_render_pass(vk::Format format, bool force
         .srcStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eLateFragmentTests,
         .dstStageMask = vk::PipelineStageFlagBits::eColorAttachmentOutput | vk::PipelineStageFlagBits::eEarlyFragmentTests,
         .srcAccessMask = vk::AccessFlagBits::eColorAttachmentWrite | vk::AccessFlagBits::eDepthStencilAttachmentWrite,
-        .dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eDepthStencilAttachmentRead
+        .dstAccessMask = vk::AccessFlagBits::eColorAttachmentRead | vk::AccessFlagBits::eColorAttachmentWrite | 
+                         vk::AccessFlagBits::eDepthStencilAttachmentRead | vk::AccessFlagBits::eDepthStencilAttachmentWrite
     };
 
+    // BUG: This is dead code (as immediately overwritten below)
     if (state.features.support_shader_interlock && no_color) {
         // we must wait for the previous shaders to be done
         dependencies[1].dstStageMask = vk::PipelineStageFlagBits::eFragmentShader;
